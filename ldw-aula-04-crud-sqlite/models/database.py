521d00b83a5aa38221876f82378665fdb0d22da8
DSM4 <fatec-dsm4@academico.intra>
2025-09-01 22:20:07 -0300

    aula 03 e 04

---
+++ ldw-aula-04-crud-sqlite/models/database.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    year = db.Column(db.Integer)
+    category = db.Column(db.String(150))
+    platform = db.Column(db.String(150))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+    
+    # Método construtor da classe
+    def __init__(self, title, year, category, platform, price, quantity):
+        self.title = title
+        self.year = year
+        self.category = category
+        self.platform = platform
+        self.price = price
+        self.quantity = quantity
